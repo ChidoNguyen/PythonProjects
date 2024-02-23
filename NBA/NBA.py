225c6703465d4603f13a692c4fe959adde879e9f
Chido Nguyen <chidonguyen@hotmail.com>
2024-02-22 19:16:01 -0800

    selenium webdriver to let nba.com/games load fully to fill in place holder

--- NBA/NBA.py
+++ NBA/NBA.py
@@ -3,6 +3,12 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 #!! All URL strings end with / but do not start with / in order to make appending for paths easier !!#
 """
 Quick script to pull today's NBA matchups 
@@ -40,25 +46,68 @@ def nba_game_path(soup, gPath):
 #*Function : matchup_div()
 #@param : 
 def matchup_div(soup):
+    sects = soup.find_all('section')
+    gamecard_sects = []
+    #html skeleton
+    #sects -> get team abbreviation from href /teama_vs_teamb
+    #traverse children to get game time
+    for items in sects:
+        #print(items.get('class')[0])
+        if 'GameCard_gcMain' in items.get('class')[0]: 
+            gamecard_sects.append(items)
+    
+    #children in sect_gc has 2 1) a and 2) ul we can skip the UL and extract the a tag
+    teams = []
+    for i in gamecard_sects:
+        a_kids = list(i.children) # tag a and tag ul 
+        teams.append(a_kids[0])
+    return
     try:
         divs = soup.find_all('div')
     except  urllib.error.URLError as e:
         print(e)
+    tmp = []
     for items in divs:
+        if 'class' in items.attrs and items.attrs['class'] : tmp.append(items.get('class'))
+    
+    return
+            
         #from our divs find extract all the div's classes and find if "Games" word is in the class descriptions
         #GamesView ________gameCard#
-        if 'class' in items.attrs and len(items.attrs['class']) > 0 and items.attrs['class'][0].find('Games') >= 0 :
-            if re.search('^(.*gameCard.*)$',items.attrs['class'][0]): print(items.name)
+        #if 'class' in items.attrs and len(items.attrs['class']) > 0 and items.attrs['class'][0].find('Games') >= 0 :
+         #   if re.search('^(.*GameCard.*)$',items.attrs['class'][0]): print(items)
 
     
-    
+#*Might needt selenium header to fully load webpage prior to souping it
+def sel_soup():
+    url = "https://www.nba.com/games"    
+    #headless
+    chrome_opts = Options()
+    chrome_opts.add_argument('--headless')
+    chrome_opts.add_argument('executable_path=E:\chromeweb\chromedriver.exe')
+    chrome_opts.add_argument('--disable-cache')
+
+    #driver
+    driver = webdriver.Chrome(options = chrome_opts)
+
+    #opening website
+
+    driver.get(url)
+    driver.refresh()
+    WebDriverWait(driver , 10)
+    final_html = driver.page_source
+    driver.quit()
+
+    soup = BeautifulSoup(final_html, 'lxml')
+    return soup
 
 def main():
     baseURL = "https://www.nba.com/"
     path_nba_games_today = "games/"
     nbaSoup = make_nba_soup(baseURL)
     score_board = nba_game_path(nbaSoup , path_nba_games_today) # returns /path 
-    _div = matchup_div(BeautifulSoup(urllib.request.urlopen(baseURL+path_nba_games_today), 'lxml'))
+    _div = matchup_div(sel_soup())
+    #_div = matchup_div(BeautifulSoup(urllib.request.urlopen(baseURL+path_nba_games_today), 'lxml'))
     #tmp = nbaSoup.find_all('data-text')
     #print(tmp)
 """
