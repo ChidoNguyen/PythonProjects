9b7c547b976dc19a553e7099ea41574b5e523566
Chido Nguyen <chidonguyen@hotmail.com>
2024-03-13 20:46:56 -0700

    button with onClick event working ; todo: add some logic/"work" that needs to be done when clicked

--- React/practice/src/CreateButton.js
+++ React/practice/src/CreateButton.js
@@ -1,11 +1,16 @@
 import React from "react";
+import './Button.css'
 //props here will be 2 int values to create our button key
 const CreateButton = (props) => {
     // make my button
     const buttKey = props.buttonKey
 
+    const handleClick = () =>{
+        alert('woah!');
+    };
+
     return (
-        <button key = {buttKey}>XO</button>
+        <button className = 'empty-button'key = {buttKey} onClick = {handleClick}></button>
     );
 }
 export default CreateButton;
