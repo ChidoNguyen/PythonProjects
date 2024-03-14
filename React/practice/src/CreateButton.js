66e81d81553e5242f3b0f7bb77a7cce7c87dac1c
Chido Nguyen <chidonguyen@hotmail.com>
2024-03-13 21:06:26 -0700

    on click does X/O filling  ; ISSUE: need player turn useState to be more global

--- React/practice/src/CreateButton.js
+++ React/practice/src/CreateButton.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React , {useState} from "react";
 import './Button.css'
 //props here will be 2 int values to create our button key
 const CreateButton = (props) => {
     // make my button
     const buttKey = props.buttonKey
+    const [playerText , setDisplayText] = useState(' '); //useState takes starting arg in this case empty string
+    const [playerTurn , setPlayerTurn] = useState(0);
 
+    // on click we check 
     const handleClick = () =>{
-        alert('woah!');
+        if( playerTurn === 0){
+            setDisplayText('X');
+        } else {
+            setDisplayText('O');
+        }
+        setPlayerTurn(1)
+        console.log(playerTurn)
     };
 
     return (
-        <button className = 'empty-button'key = {buttKey} onClick = {handleClick}></button>
+        <button className = 'empty-button'key = {buttKey} onClick = {handleClick}>
+            <p>{playerText}</p>
+        </button>
     );
 }
 export default CreateButton;
