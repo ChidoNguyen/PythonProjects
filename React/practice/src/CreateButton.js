9bba798cc8b35876ea2f9c3277037d8028dcac30
Chido Nguyen <chidonguyen@hotmail.com>
2024-03-16 10:13:27 -0700

    pre-async

--- React/practice/src/CreateButton.js
+++ React/practice/src/CreateButton.js
@@ -1,25 +1,37 @@
 import React , {useState} from "react";
 import './Button.css'
 //props here will be 2 int values to create our button key
+
 const CreateButton = (props) => {
     // make my button
     const buttKey = props.buttonKey
-    const [playerText , setDisplayText] = useState(' '); //useState takes starting arg in this case empty string
-    const [playerTurn , setPlayerTurn] = useState(0);
+    const [playerText , setDisplayText] = useState(''); //useState takes starting arg in this case empty string
 
     // on click we check 
     const handleClick = () =>{
-        if( playerTurn === 0){
-            setDisplayText('X');
-        } else {
-            setDisplayText('O');
+        if(playerText === ''){
+            if (props.count === 0){
+                setDisplayText('X');
+            }else{
+                setDisplayText('O');
+            }
+            props.move(buttKey);
+            
+            //let pC = props.count === 0 ? props.p1: props.p2;
+            //console.log(props.p1);
+            //console.log(props.p2);
+            if (props.win(pC)) {
+                window.location.reload()
+                //winner_restart();
+            }
+            props.turn();
+        } else{
+            alert("Can't make a play here!");
         }
-        setPlayerTurn(1)
-        console.log(playerTurn)
     };
 
     return (
-        <button className = 'empty-button'key = {buttKey} onClick = {handleClick}>
+        <button className = 'empty-button' key = {buttKey} onClick = {handleClick}>
             <p>{playerText}</p>
         </button>
     );
--- React/practice/src/CreateTable.js
+++ React/practice/src/CreateTable.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React , {useState} from "react";
 import config from  './config';
 import './Table.css'
 import CreateButton from './CreateButton';
 /* could prolly pass in X_Y grid size*/
+/*could be considered our "game" controller too */
 
+const WinCon = (playerChoice) => {
+    // 00 / 01 // 02
+    // 10 / 11 // 12
+    // 20 / 21 // 22
+    // 3x3 square ids
+    const winDex = [["00","01","02"],["00","10","20"],["10","11","12"],["20","21","22"],["01","11","21"],
+    ["02","12","22"],["00","11","22"],["02","11","20"]];
+    const arr = playerChoice;
+    for (let i = 0; i < winDex.length; i++){
+        for (let j = 0; j < 3; j++){
+            if (!arr.includes(winDex[i][j])) {break;}
+            if (j == 2) {return true}
+        }
+    }
+    return false;
+
+}
 const CreateTable = () =>{
     const { numRows , numCols } = config; // import our row/col size from ocnfig file
     const rows = Array.from({length:numRows},(unusedvariable_equivalent_to_cur_index,rowIndex)=>rowIndex);
@@ -13,6 +31,31 @@ const CreateTable = () =>{
     // () => arrow function the ( unused , what_we_want) // unused reps the value of current element we dont care for it so _ , what_we_want rep the current index
     // TLDR : (_,r) idc what value is gimme the index (r)
     const keygen = (a,b) => {return `${a}${b}`}; // create our function to do a job ( in this case make a key) , call this inside our <html> identifier and pass in args see <button>
+    const [pOne, setPOne] = useState([]);
+    const [pTwo,setPTwo] = useState([])
+    const [turnCount , setTurnCount ] = useState(0);
+    const [winner, setWiner] = useState(0); // 0 no win / 1 is p1 , 2 is p2
+
+    const changeTurns = () =>{
+        if (turnCount === 0){
+            setTurnCount(1);
+        } else {
+            setTurnCount(0);
+        }
+        //console.log(turnCount)
+        // game winning logic check here
+    };
+    const makeMove = (boxKey) =>{
+        if(turnCount === 0){
+            // recommended to not mutate original but make new one
+            const newMoves = [...pOne,boxKey]
+            setPOne(newMoves)
+        } else {
+            const newMoves = [...pTwo,boxKey]
+            setPTwo(newMoves)
+        }
+    };
+
     return(
         <table className = "board">
             <tbody>
@@ -20,7 +63,7 @@ const CreateTable = () =>{
                     <tr key = {rowIndex}>
                         {cols.map( colIndex =>(
                             <td key = {colIndex}>
-                                <CreateButton buttonKey = {keygen(rowIndex,colIndex)} />
+                                <CreateButton p1 = {pOne}  p2 = {pTwo} buttonKey = {keygen(rowIndex,colIndex)} count = {turnCount} turn = {changeTurns} move = {makeMove} win = {WinCon} />
                             </td>
                         ))}
                     </tr>
